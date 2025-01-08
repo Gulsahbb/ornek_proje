db0166b779159b119087af68dbbd15b3e3e09efa
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-08 17:28:47 +0300

    scaffold devam

--- lib/101/text_learn_view.dart
+++ lib/101/text_learn_view.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
 class TextLearnView extends StatelessWidget {
+  const TextLearnView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Text('Gulsah');
+    return Scaffold(
+      body: Center(
+          child: Text(('Buy The Best One' * 10),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  decoration: TextDecoration.underline,
+                  wordSpacing: 2,
+                  letterSpacing: 4,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold))),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-// Stateless Widget : Sabit Ekran , Sabit Görünüm
-// Uygulama ilk başladığında bir görünüm ister.
 import 'package:flutter/material.dart';
+import 'package:ornek_proje/101/text_learn_view.dart';
 
 void main() {
   runApp(const Uygulamam());
@@ -8,69 +7,12 @@ void main() {
 
 class Uygulamam extends StatelessWidget {
   const Uygulamam({super.key});
-/* 
-Uygulama çalışır ama görünümü nasıl belirtmek gerekiyor.
-   - Android için Material App görünümünü kullanmalıyız. 
-*/
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(fontFamily: "Nunito"),
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                'Uygulamam',
-                style:
-                    TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
-              ),
-              backgroundColor: Colors.green,
-              centerTitle: true,
-            ),
-            body: Center(
-              child: Column(
-                children: [
-                  Container(
-                      padding: const EdgeInsets.all(80),
-                      margin: const EdgeInsets.all(80),
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey.shade400,
-                                blurRadius: 6,
-                                spreadRadius: 1),
-                          ]),
-                      child: Text(
-                        'Test App',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25),
-                      )),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: Image.asset('assets/images/dinoinspace.jpeg',
-                        height: 100),
-                  ),
-                  SizedBox(
-                    height: 80,
-                    width: 200,
-                    child: Card(
-                        color: Colors.green,
-                        elevation: 3,
-                        child: Align(
-                          child: Text(
-                            'Merhaba',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600),
-                          ),
-                        )),
-                  )
-                ],
-              ),
-            )));
+        title: 'Flutter Demo',
+        theme: ThemeData.dark(),
+        home: const TextLearnView());
   }
 }
