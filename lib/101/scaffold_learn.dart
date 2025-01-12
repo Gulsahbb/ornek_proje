8b28379899d360dc554737fd5e053d72f016aaad
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-12 22:33:47 +0300

    Create Scaffold View

--- lib/101/scaffold_learn.dart
+++ lib/101/scaffold_learn.dart
@@ -6,8 +6,18 @@ class ScaffoldLearnView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Text('Merhaba'),
+      appBar: AppBar(
+        title: const Text('Scaffold Samples',
+            style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: const Text('Merhaba', style: TextStyle(color: Colors.black)),
+      backgroundColor: Colors.white60,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blueGrey,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomNavigationBar(items: const [
         BottomNavigationBarItem(icon: Icon(Icons.abc_outlined), label: 'a'),
         BottomNavigationBarItem(icon: Icon(Icons.abc_outlined), label: 'b'),
