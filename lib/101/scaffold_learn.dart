14188792d5a02dfdf7e8ebd5c2ae6ad4e30ffc8e
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-12 22:22:30 +0300

    Create Scaffold View

---
+++ lib/101/scaffold_learn.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ScaffoldLearnView extends StatelessWidget {
+  const ScaffoldLearnView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Text('Merhaba'),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.abc_outlined), label: 'a'),
+        BottomNavigationBarItem(icon: Icon(Icons.abc_outlined), label: 'b'),
+      ]),
+    );
+  }
+}
