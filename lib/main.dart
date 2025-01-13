46190150b965a2392214f5d3820cfb4948fd5008
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-13 12:59:45 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ornek_proje/101A/stateless_widget_test.dart';
 
 // import 'package:ornek_proje/101/container_sized_box_learn.dart';
-import 'package:ornek_proje/101/scaffold_learn.dart';
+// import 'package:ornek_proje/101/scaffold_learn.dart';
 // import 'package:ornek_proje/101/text_learn_view.dart';
 
 void main() {
@@ -17,6 +18,17 @@ class Uygulamam extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData.dark(),
-        home: ScaffoldLearnView());
+        // home: ScaffoldLearnView())
+        home: StatelessWidgetTest());
   }
 }
+/*
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData.dark(),
+        home: ScaffoldLearnView());
+  }
+*/
