6feaadb005d63f35d654b6bf3f7096f484859c65
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-13 16:59:46 +0300

    Create Container

--- lib/101A/stateless_widget_test.dart
+++ lib/101A/stateless_widget_test.dart
@@ -5,6 +5,63 @@ class StatelessWidgetTest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.white54,
+        shadowColor: Colors.deepPurple,
+      ),
+      appBar: AppBar(
+        /* leading: IconButton(
+            icon: Icon(Icons.menu, color: Colors.white), onPressed: () {}),*/
+        title: Text(
+          'Deneme',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple.shade200,
+        actions: [
+          Icon(
+            Icons.arrow_forward_rounded,
+            color: Colors.white,
+            weight: 20,
+          )
+        ],
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 200,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withValues(),
+                spreadRadius: 1,
+                blurRadius: 5,
+                offset: Offset(0, 2),
+              )
+            ],
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.deepPurple.shade200,
+          ),
+          child: Text('Merhaba'),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.deepPurple.shade200,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), label: 'Account'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.all_inbox_rounded), label: 'Inbox'),
+        ],
+      ),
+    );
   }
 }
