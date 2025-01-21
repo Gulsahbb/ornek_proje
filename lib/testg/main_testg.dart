90a3ed6cd6d5251b1a5fcdc440565fe6294ba785
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-21 21:56:41 +0300

    Created main_testg.dart

---
+++ lib/testg/main_testg.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text('TEST', style: TextStyle(color: Colors.white)),
+      ),
+      body: SizedBox(
+        width: 300,
+        height: 300,
+        child: Text('Deneme Sayfası'),
+      ),
+    );
+  }
+}
