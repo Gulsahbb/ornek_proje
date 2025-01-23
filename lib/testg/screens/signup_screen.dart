d9c6d05ceac1062499f59d955193dcbe8a0b2b4b
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-23 22:19:07 +0300

    Add sign up

--- lib/testg/screens/signup_screen.dart
+++ lib/testg/screens/signup_screen.dart
@@ -6,7 +6,89 @@ class SignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Kayıt Olma Sayfası'),
+      appBar: AppBar(
+        title: Text('Kayıt Ol'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(children: [
+            TextField(
+              decoration: InputDecoration(
+                  labelText: 'Ad Soyad',
+                  border: OutlineInputBorder(),
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black))),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  labelText: 'E-posta',
+                  border: OutlineInputBorder(),
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black))),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  labelText: 'Şifre',
+                  border: OutlineInputBorder(),
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black))),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  labelText: 'Şifre Tekrar',
+                  border: OutlineInputBorder(),
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black))),
+            ),
+            SizedBox(height: 30),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black),
+                onPressed: () {
+                  // Kayıt işlemi için bir fonksiyon
+                  print('Kayıt İşlemi Başlatıldı!');
+                },
+                child: Text(
+                  'Kayıt Ol',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+            Center(
+                child: TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                'Zaten bir hesabınız var mı? Giriş Yap',
+                style: TextStyle(fontSize: 16, color: Colors.grey.shade700),
+              ),
+            ))
+          ]),
+        ),
+      ),
     );
   }
 }
