36cd536e74bdf1c9aa8b2163a1b9d871ed96254f
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-24 17:54:10 +0300

    Add Forgot Password items

--- lib/testg/screens/forgot_password_screen.dart
+++ lib/testg/screens/forgot_password_screen.dart
@@ -1,43 +1,58 @@
 import 'package:flutter/material.dart';
 
-class ForgotPasswordScreen extends StatelessWidget {
+class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
 
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  String _email = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(18.0),
+        padding: EdgeInsets.all(18.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text('Şifre Sıfırlama',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 20),
-            const Text(
+            SizedBox(height: 20),
+            Text(
               'Lütfen şifre sıfırlama bağlantısını alabilmek için e-posta adresinizi girin.',
               style: TextStyle(fontSize: 16),
             ),
-            const SizedBox(height: 20),
-            const TextField(
+            SizedBox(height: 20),
+            TextField(
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
                 labelText: 'E-posta',
                 border: OutlineInputBorder(),
                 hintText: 'E-posta adresinizi girin',
               ),
+              onChanged: (value) {
+                setState(() {
+                  _email = value;
+                });
+              },
             ),
-            const SizedBox(height: 30),
+            SizedBox(height: 30),
             ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
+                    backgroundColor:
+                        _email.isEmpty ? Colors.white : Colors.grey,
                     foregroundColor: Colors.black,
                     padding: EdgeInsets.all(20)),
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text('Şifre Sıfırlama Bağlantısı Gönderildi.')));
-                },
+                onPressed: _email.isEmpty
+                    ? null
+                    : () {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(
+                                'Şifre Sıfırlama Bağlantısı Gönderildi.')));
+                      },
                 child: Text('Bağlantı Gönder'))
           ],
         ),
