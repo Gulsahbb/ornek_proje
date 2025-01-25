51e64c4ccc20ef517de1c940b7178cd94b61b84a
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-25 13:02:38 +0300

    Add Forgot Password Screen feat

--- lib/testg/screens/forgot_password_screen.dart
+++ lib/testg/screens/forgot_password_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ornek_proje/testg/theme/app_theme.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
@@ -12,26 +13,66 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppTheme.accentColor,
+        toolbarHeight: 100,
+      ),
+      backgroundColor: AppTheme.backgroundColor,
       body: Padding(
         padding: EdgeInsets.all(18.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Şifre Sıfırlama',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            Text(
+              'Şifre Sıfırlama',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: AppTheme.textColor,
+                fontFamily: 'Montserrat',
+              ),
+            ),
             SizedBox(height: 20),
             Text(
               'Lütfen şifre sıfırlama bağlantısını alabilmek için e-posta adresinizi girin.',
-              style: TextStyle(fontSize: 16),
+              style: TextStyle(
+                fontSize: 16,
+                color: AppTheme.softGrayColor,
+                fontFamily: 'Montserrat',
+              ),
             ),
             SizedBox(height: 20),
             TextField(
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
                 labelText: 'E-posta',
-                border: OutlineInputBorder(),
                 hintText: 'E-posta adresinizi girin',
+                labelStyle: TextStyle(color: AppTheme.textColor),
+                filled: true,
+                fillColor: Colors.white,
+                prefixIcon:
+                    Icon(Icons.person_outline, color: AppTheme.textColor),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: AppTheme.softGrayColor,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: AppTheme.softGrayColor,
+                    width: 1.5,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: AppTheme.textColor,
+                    width: 1.8,
+                  ),
+                ),
               ),
               onChanged: (value) {
                 setState(() {
@@ -43,10 +84,15 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
             // Bağlantı GÖnder Butonu
             ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        _email.isEmpty ? Colors.white : Colors.grey,
-                    foregroundColor: Colors.black,
-                    padding: EdgeInsets.all(20)),
+                  backgroundColor: _email.isEmpty
+                      ? AppTheme.textColor
+                      : AppTheme.softGrayColor,
+                  foregroundColor: AppTheme.backgroundColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                ),
                 onPressed: _email.isEmpty
                     ? null
                     : () {
@@ -54,7 +100,14 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                             content: Text(
                                 'Şifre Sıfırlama Bağlantısı Gönderildi.')));
                       },
-                child: Text('Bağlantı Gönder'))
+                child: Text(
+                  'Bağlantı Gönder',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontFamily: 'Montserrat',
+                  ),
+                ))
           ],
         ),
       ),
