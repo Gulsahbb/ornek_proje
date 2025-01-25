a68cf6b63b9902208b38ace636f5d509711603bc
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-25 12:19:45 +0300

    Create App Theme

---
+++ lib/testg/theme/app_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF3D348B); // Koyu mavi
+  static const Color secondaryColor = Color(0xFF7678ED); // Açık mavi
+  static const Color warningColor = Color(0xFFF7B801); // Altın sarısı
+  static const Color accentColor = Color(0xFFF18701); // Turuncu
+  static const Color errorColor = Color(0xFFF35B04); // Koyu turuncu
+  static const Color backgroundColor =
+      Color(0xFFFFFFFF); // Açık gri (arka plan)
+  static const Color textColor = Color(0xFF4E4E4E); // Füme (yazı rengi)
+  static const Color softGrayColor =
+      Color(0xFF7D7D7D); // Gri (yardımcı yazı rengi)
+}
