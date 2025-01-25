71b2a9fe9d6a74c9973ecd17c4ecd6ebd5aa59bc
Gulsahbb <gulsahbaskurt.10@gmail.com>
2025-01-25 12:52:53 +0300

    Update Signup Screen Style feat

--- lib/testg/screens/signup_screen.dart
+++ lib/testg/screens/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ornek_proje/testg/screens/login_screen.dart';
+import 'package:ornek_proje/testg/theme/app_theme.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -14,110 +15,216 @@ class _SignupScreenState extends State<SignupScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Kayıt Ol'),
+        elevation: 0,
+        backgroundColor: AppTheme.accentColor,
+        toolbarHeight: 100, // AppBar yüksekliği
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(children: [
-            TextField(
-              decoration: InputDecoration(
-                  labelText: 'Ad Soyad',
-                  border: OutlineInputBorder(),
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black))),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  labelText: 'E-posta',
-                  border: OutlineInputBorder(),
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black))),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  labelText: 'Şifre',
-                  border: OutlineInputBorder(),
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black))),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  labelText: 'Şifre Tekrar',
-                  border: OutlineInputBorder(),
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black))),
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                Checkbox(
-                  value: sartKabulu,
-                  onChanged: (value) {
-                    setState(() {
-                      sartKabulu = value ?? false;
-                    });
-                  },
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
+          child: Column(
+            children: [
+              // Başlık
+              Text(
+                "Kayıt Ol",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: AppTheme.textColor,
+                  fontFamily: 'Montserrat',
                 ),
-                Expanded(
-                  child: Text(
-                    "Kayıt olarak kullanım şartlarını kabul etmiş sayılırsınız.",
-                    style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Hesabınızı oluşturun ve platformumuza katılın",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: AppTheme.softGrayColor,
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+              SizedBox(height: 40),
+              // Ad Soyad TextField
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Ad Soyad",
+                  labelStyle: TextStyle(color: AppTheme.textColor),
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon:
+                      Icon(Icons.person_outline, color: AppTheme.softGrayColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.softGrayColor,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.softGrayColor,
+                      width: 1.5,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.textColor,
+                      width: 1.8,
+                    ),
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
+              ),
+              SizedBox(height: 20),
+              // E-mail TextField
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "E-posta",
+                  labelStyle: TextStyle(color: AppTheme.textColor),
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon:
+                      Icon(Icons.email_outlined, color: AppTheme.softGrayColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.softGrayColor,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.softGrayColor,
+                      width: 1.5,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.textColor,
+                      width: 1.8,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              // Şifre TextField
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Şifre",
+                  labelStyle: TextStyle(color: AppTheme.textColor),
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon:
+                      Icon(Icons.lock_outline, color: AppTheme.softGrayColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.softGrayColor,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.softGrayColor,
+                      width: 1.5,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.textColor,
+                      width: 1.8,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              // Şifre Onay TextField
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Şifre Onay",
+                  labelStyle: TextStyle(color: AppTheme.textColor),
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: Icon(Icons.lock_reset_outlined,
+                      color: AppTheme.softGrayColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.softGrayColor,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.softGrayColor,
+                      width: 1.5,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: AppTheme.textColor,
+                      width: 1.8,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              // Kayıt Ol Butonu
+              ElevatedButton(
+                onPressed: () {
+                  // Kayıt olma işlemi
+                },
                 style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black),
-                onPressed: sartKabulu
-                    ? () {
-                        // Kullanım şartları kabul edildiyse kayıt işlemi yapılabilir
-                        print('Kayıt ol işlemi yapılıyor...');
-                      }
-                    : null,
-                child: Text(
-                  'Kayıt Ol',
-                  style: TextStyle(fontSize: 18),
+                  backgroundColor: AppTheme.textColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                ),
+                child: Center(
+                  child: Text(
+                    "Kayıt Ol",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontFamily: 'Montserrat',
+                    ),
+                  ),
                 ),
               ),
-            ),
-            Center(
-                child: TextButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => LoginScreen()));
-              },
-              child: Text(
-                'Zaten bir hesabınız var mı? Giriş Yap',
-                style: TextStyle(fontSize: 16, color: Colors.grey.shade700),
+              SizedBox(height: 10),
+              // Giriş Yap Linki
+              TextButton(
+                style: TextButton.styleFrom(
+                    textStyle: TextStyle(decoration: TextDecoration.underline)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Zaten bir hesabınız var mı? Giriş Yap",
+                  style: TextStyle(
+                    color: AppTheme.softGrayColor,
+                    fontSize: 14,
+                    fontFamily: 'Montserrat',
+                  ),
+                ),
               ),
-            ))
-          ]),
+            ],
+          ),
         ),
       ),
+      backgroundColor: Colors.white,
     );
   }
 }
